b2fcc52c248bf5675a7f4970671857447ad55aac
Ihor Poliakov <piv2003@gmail.com>
2024-03-01 21:18:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import './sass/index.scss';
-
 import './js/footer-modal';
-
 import './js/genre';
-
 import './js/news-api';
-
 import './js/search-query';
-
 import './js/pagination';
-
 import './js/modalOnClick';
-
 import './js/lightSwitch';
-
 import './js/btn-up';
